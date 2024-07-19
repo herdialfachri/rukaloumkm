8d8159a8c1e2a8aef3117fdee53b76f3f42edcb6
herdialfachri <alfachri75@gmail.com>
2024-07-19 23:16:15 +0700

    fix bug in searchview

--- app/src/main/java/com/herdialfachri/rukaloumkm/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/herdialfachri/rukaloumkm/ui/home/HomeViewModel.kt
@@ -8,13 +8,16 @@ import com.herdialfachri.rukaloumkm.data.DataClass
 
 class HomeViewModel : ViewModel() {
 
+    // LiveData untuk menyimpan daftar produk
     private val _products = MutableLiveData<List<DataClass>>()
     val products: LiveData<List<DataClass>> get() = _products
 
+    // Referensi ke database Firebase untuk node "Product"
     private val databaseReference: DatabaseReference = FirebaseDatabase.getInstance().getReference("Product")
     private val eventListener: ValueEventListener
 
     init {
+        // EventListener untuk mendengarkan perubahan data pada node "Product"
         eventListener = object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val productList = mutableListOf<DataClass>()
@@ -27,16 +30,19 @@ class HomeViewModel : ViewModel() {
             }
 
             override fun onCancelled(error: DatabaseError) {
-                // Handle error
+                // Tangani kesalahan
             }
         }
+        // Menambahkan event listener ke referensi database
         databaseReference.addValueEventListener(eventListener)
     }
 
+    // Fungsi untuk menyegarkan data dengan mengambil data sekali dari database
     fun refreshData() {
         databaseReference.addListenerForSingleValueEvent(eventListener)
     }
 
+    // Membersihkan event listener ketika ViewModel dihapus
     override fun onCleared() {
         super.onCleared()
         databaseReference.removeEventListener(eventListener)
